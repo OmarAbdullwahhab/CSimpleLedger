5a9f64a3efb98802a92c2f34751d564aa591c2b0
Omar AbdullWahhab <omar.abdullwahhab@gmail.com>
2018-03-07 14:23:21 +0300

    Added financial statement definition

---
+++ SimpleLedger/LedgerModel/Reporting/FinancialStatement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LedgerModel.Ledger.Reporting
+{
+    /// <summary>
+    /// This class is used to represent a  user defined financial statement instead 
+    /// of defining it statically
+    /// </summary>
+    public class FinancialStatement
+    {
+        public int ID
+        {
+            get;set;
+        }
+
+        /// <summary>
+        /// The title that will be displayed in the financial statement
+        /// for extample
+        /// 
+        ///                                Company Name Here
+        ///                                  Balance Sheet
+        ///                                December 31, 2010
+        /// or
+        ///                                Company Name Here
+        ///                                 Income Statement 
+        ///                        for the year ended December 31, 2017
+        /// 
+        /// </summary>
+        public string Title
+        {
+            get;set;
+        }
+
+        public ICollection<FinancialStatementItem> StatementItems
+        {
+            get;set;
+        }
+
+    }
+}
---
+++ SimpleLedger/LedgerModel/Reporting/FinancialStatementItem.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LedgerModel.Ledger.Reporting
+{
+
+    public class FinancialStatementItem
+    {
+        public int ID
+        {
+            get;set;
+        }
+
+        /// <summary>
+        /// The order to display this item in the financial statement
+        /// </summary>
+        public int DisplayIndex
+        {
+            get;set;
+        }
+
+        public string Name
+        {
+            get;set;
+        }
+
+        public ICollection<LedgerAccount> Accounts
+        {
+            get;set;
+        }
+
+        /// <summary>
+        /// if is set to true the this item will have sub total in the statement..
+        /// for example 
+        /// Total Current Assets.....................................15045555.55
+        /// </summary>
+        public bool DisplaySubTotal
+        {
+            get;set;
+        }
+
+        /// <summary>
+        /// If this item has many accounts and this property is set to false 
+        /// the accounts will not be displayed in the statement.
+        /// </summary>
+        public bool DisplayAccounts
+        {
+            get;set;
+        }
+
+        /// <summary>
+        /// The financial statement this item belongs to.
+        /// </summary>
+        public FinancialStatement Statement
+        {
+            get;set;
+        }
+
+        /// <summary>
+        /// 
+        /// The root item of this item.
+        /// </summary>
+        public FinancialStatementItem ParentItem
+        {
+            get;set;
+        }
+
+        /// <summary>
+        /// The children items of this item.
+        /// </summary>
+        public ICollection<FinancialStatementItem> SubItems
+        {
+            get;set;
+        }
+    }
+}
